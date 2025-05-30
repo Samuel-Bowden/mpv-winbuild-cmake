cb0739921e1e442a2a479531cc842cff21cc6e18
Sam Bowden <sam.bowden@techex.co.uk>
2025-05-30 19:31:37 +0100

    fix: try specifying branch

--- packages/vulkan-header.cmake
+++ packages/vulkan-header.cmake
@@ -4,7 +4,7 @@ ExternalProject_Add(vulkan-header
     GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     GIT_REMOTE_NAME origin
-    GIT_TAG v1.4.315
+    GIT_TAG vulkan-sdk-1.4.313
     CONFIGURE_COMMAND ${EXEC} CONF=1 cmake -H<SOURCE_DIR> -B<BINARY_DIR>
         -G Ninja
         -DCMAKE_BUILD_TYPE=Release
